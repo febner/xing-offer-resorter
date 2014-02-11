2ca6afa6d9f0f04245ec2980d4ba8503cb24e7ea
febner <febner1@gmail.com>
2014-02-12 00:00:14 +0100

    + initial commit

---
+++ src/main/java/Resorter.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * 
+ */
+
+/**
+ * @author Franz
+ * 
+ */
+public class Resorter {
+
+    private Resorter() {
+
+    }
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.xing.com");
+
+        // logon
+
+        // input orffers as stated in a config file
+
+    }
+
+}
